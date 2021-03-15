a11da4c660544a6902348360ddbf51a2d5d7ea8a
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-03-14 21:03:09 -0400

    logo added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import mainLogo from "../assets/moon-transparent.png"
 
 export default function Header() {
   return (
     <header className="header">
-      <h1>Logo</h1>
+      <img src={mainLogo} alt="rocket to bitcoin"/>
       <button>Light</button>
       <nav>
         <Link to="/all">ALL</Link>
